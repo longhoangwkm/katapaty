f51978e59d2d3696895e3018feae16566b0145b9
longhoang.wkm <longhoang@wakumo.vn>
2018-10-23 14:44:04 +0700

    Add btc_balance method

--- lib/katapaty/party.rb
+++ lib/katapaty/party.rb
@@ -17,29 +17,37 @@ module Katapaty
       end
 
       def get_asset_info(assets)
-        request(__method__.to_s, { assets: assets })
+        request(__method__, { assets: assets })
       end
 
       def get_blocks(block_indexes)
-        request(__method__.to_s, { block_indexes: block_indexes })
+        request(__method__, { block_indexes: block_indexes })
       end
 
       def getrawtransaction(tx_hash)
-        request(__method__.to_s, { tx_hash: tx_hash })
+        request(__method__, { tx_hash: tx_hash })
       end
 
       def create_send(payload)
-        request(__method__.to_s, payload)
+        request(__method__, payload)
+      end
+
+      def btc_unconfirm_balance(address)
+        get_unspent_txouts(address).map { |a| a['amount'] }.sum.to_d * 1e8).to_i
+      end
+
+      def get_unspent_txouts(address)
+        request(__method__, { address: address, unconfirmed: true })
       end
 
       def request(method_name, payload={})
         client = RestClient::Resource.new Katapaty.configuration.counterparty_url
-        request = { method: method_name, params: payload, jsonrpc: '2.0', id: '0' }.to_json
+        request = { method: method_name.to_s, params: payload, jsonrpc: '2.0', id: '0' }.to_json
         response = JSON.parse client.post(request,
                       accept: 'json',
                       content_type: 'json' )
-        raise JsonResponseError.new response if response.has_key? 'code'
-        raise ResponseError.new response['error'] if response.has_key? 'error'
+        raise Katapaty::JsonResponseError.new response if response.has_key? 'code'
+        raise Katapaty::ResponseError.new response['error'] if response.has_key? 'error'
         response['result']
       end
 
