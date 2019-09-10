009980340479c3374afbb8fa741f37cc34733c2e
longhoang.wkm <longhoang@wakumo.vn>
2019-09-10 16:19:55 +0700

    Remove redundant code

--- spec/support/rpc_service_helper.rb
+++ spec/support/rpc_service_helper.rb
@@ -1,13 +1,10 @@
 module RPCServiceHelper
-  def service(name, &block)
+  def service(method_name, &block)
     context "'#{name}'" do
-      define_method :fixture_name do
-        name
-      end
 
       define_method :result do |*args|
-        FakeWeb.register_uri(:post, "http://user:pass@localhost:14000/api/", :response => fixture(fixture_name))
-        Katapaty::Party.send(name, *args)
+        FakeWeb.register_uri(:post, "http://user:pass@localhost:14000/api/", :response => fixture(method_name))
+        Katapaty::Party.send(method_name, *args)
       end
 
       instance_eval &block
