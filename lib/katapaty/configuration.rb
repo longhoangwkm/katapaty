1ebae851c933561f91f7aaab59f689ab9fe029cf
longhoang.wkm <longhoang@wakumo.vn>
2020-08-19 11:54:53 +0700

    fix(authen): fix invalid URI error
    - Fix error when pw contain special character
    - CGI.escape

--- lib/katapaty/configuration.rb
+++ lib/katapaty/configuration.rb
@@ -12,13 +12,13 @@ module Katapaty
 
     def counterparty_url
       return 'http://rpc:1234@public.coindaddy.io:14000/api/' unless @host
-      "#{protocol}://#{@username}:#{@password}@#{@host}:#{@port}/api/"
+      "#{protocol}://#{@username}:#{CGI.escape(@password)}@#{@host}:#{@port}/api/"
     end
 
     def counterblock_url
       return 'http://rpc:1234@public.coindaddy.io:14100/api/' unless @block_host
       authen = ''
-      authen = "#{@block_username}:#{@block_password}@" if @block_username
+      authen = "#{@block_username}:#{CGI.escape(@block_password)}@" if @block_username
       "#{protocol}://#{authen}#{@block_host}:#{@block_port}/api/"
     end
 
